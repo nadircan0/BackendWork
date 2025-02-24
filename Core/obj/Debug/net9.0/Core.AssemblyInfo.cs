694172eaedac4f047297c75dce198560382f491f
Nadir Can <nadircan2003@gmail.com>
2025-02-24 19:26:16 +0300

    Auto Update

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d34b248534e8a829379e1026625400c94398057c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b972d3c6b2c38eba74615fe43b5e9d6f4c21e4aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
